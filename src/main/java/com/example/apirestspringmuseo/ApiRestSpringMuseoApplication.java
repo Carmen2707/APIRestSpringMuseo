6aeea81e01cfd0fd254e00d055e80014e52c6b15
Carmen2707 <carmengarciagr27@gmail.com>
2024-02-09 12:26:02 +0100

    Initial commit

---
+++ src/main/java/com/example/apirestspringmuseo/ApiRestSpringMuseoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apirestspringmuseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringMuseoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestSpringMuseoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/apirestspringmuseo/Museo.java
@@ -0,0 +1,2 @@
+package com.example.apirestspringmuseo;public class Museo {
+}
